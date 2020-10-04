c0b37d9e4968fa3b245ec72f3016a544f6be407f
Fedya Tsarenko <fedya132465@gmail.com>
2020-10-04 16:09:47 +0300

    finished

---
+++ main.py
@@ -0,0 +1,11 @@
+y = float(input('Y = '))
+x = float(input('X = '))
+maxNumb = y
+
+if x == y:
+    print('Числа рівні')
+elif x > y:
+    maxNumb = x
+    print('Найбільше число з двох - ', maxNumb)
+else:
+    print('Найбільше число з двох - ', maxNumb)
